9aefb3572bdfd8baed59f44fc63e434b6cf42eb3
morishima311 <ryoryo311@gmail.com>
2018-11-04 18:54:46 +0900

    add config file

--- sensor/sensor.py
+++ sensor/sensor.py
@@ -1,41 +1,47 @@
 from datetime import datetime
 from collections import OrderedDict
+from pytz import timezone
 from datetime import datetime
 import subprocess as sb
 import time as tm
 import sys, json, random
 
-if(len(sys.argv) != 4):
-    print('Usage: ".py [ip_address] [port] [num of data to send]"')
+if(len(sys.argv) != 5):
+    print('Usage: ".py [path to json config file] [ip_address] [port] [num of data to send]"')
     sys.exit()
 
 SLP_TIME = 3
-IP = sys.argv[1]
-PORT = sys.argv[2]
-SEND_NUM = int(sys.argv[3])
+PATH = sys.argv[1]
+IP = sys.argv[2]
+PORT = sys.argv[3]
+SEND_NUM = int(sys.argv[4])
 
 def netcat(ipaddr, port):
     cmd = 'nc %s %s' % (ipaddr, port)
     return sb.Popen(cmd.split(), stdin=sb.PIPE, encoding='utf8')
 
-ncPipe = netcat(IP, PORT)
+config_file = open(PATH, "r");
+config_dict = json.loads(config_file.read())
 
 measurement = "temperature"
-deviceID = "MOKA-01"
-region = 1
+host = config_dict["host"]
+region = config_dict["region"]
+
+ncPipe = netcat(IP, PORT)
 
 for i in range(SEND_NUM):
 
-    time = datetime.now().replace(microsecond=0).isoformat()
+    #time = datetime.now().replace(microsecond=0).isoformat()
+    time = datetime.now(timezone('Asia/Tokyo'))
     temperature = random.randint(0,100)
 
     json_body = {
         "measurement": measurement,
         "tags": {
-            "host": deviceID,
+            "host": host,
             "region": region
         },
-        "time": time,
+        "time": str(time),
         "fields": {
             "value": temperature,
         }
