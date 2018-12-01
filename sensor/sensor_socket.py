43e7bd72eeab26fb1d90f8d9a7ec82d6fa7c91e9
morishima311 <ryoryo311@gmail.com>
2018-12-01 17:40:44 +0900

    Changed JSON to YAML config file

--- sensor/sensor_socket.py
+++ sensor/sensor_socket.py
@@ -2,43 +2,45 @@ from pytz import timezone
 from datetime import datetime
 import time as tm
 import numpy as np
-import socket, sys, json, random
+import socket, sys, json, random, yaml
 
 if(len(sys.argv) != 2):
     print('Usage: ".py [path to json config file]"')
     sys.exit()
 
 PATH = sys.argv[1]
-config_file = open(PATH, "r");
-config_dict = json.loads(config_file.read())
+configFile = open(PATH, "r");
+configDict = yaml.load(configFile.read())
 
-HOST = config_dict["host"]
-REGION = config_dict["region"]
-PORT = config_dict["dst port"]
-IP = config_dict["dst ip"]
-SEND_NUM = config_dict["num"]
-SLP_TIME = config_dict["sleep time"]
+host = configDict["host"]
+region = configDict["region"]
+dstIp = configDict["dstIp"]
+dstPort = configDict["dstPort"]
+numberOfData = configDict["numberOfData"]
+numberOfSocket = configDict["numberOfSocket"];
+sleepTime = configDict["sleepTime"]
 
 measurement = "temperature"
 mu, sigma = 25, 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((IP, PORT))
+s.connect((dstIp, dstPort))
 
-for i in range(SEND_NUM):
+for i in range(numberOfData):
 
     time = datetime.now(timezone('Asia/Tokyo'))
-    temperature = np.random.normal(mu, sigma, 1)
+    #temperature = int(np.random.normal(mu, sigma, 1))
+    temperature = random.randint(0,100)
 
     json_body = {
         "measurement": measurement,
         "tags": {
-            "host": HOST,
-            "region": REGION
+            "host": host,
+            "region": region
         },
         "time": str(time),
         "fields": {
-            "value": int(temperature[0]),
+            "value": temperature,
         }
     }
 
@@ -46,4 +48,4 @@ for i in range(SEND_NUM):
     msg += "\n"
 
     s.send(msg.encode('utf-8'))
-    tm.sleep(SLP_TIME)
+    tm.sleep(sleepTime)
