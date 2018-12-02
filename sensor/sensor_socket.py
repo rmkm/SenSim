54c2fa3f95875744af97f2eecebb6fade6ed615d
morishima311 <ryoryo311@gmail.com>
2018-12-02 10:28:24 +0900

    Enabled to choose number of socket to create

--- sensor/sensor_socket.py
+++ sensor/sensor_socket.py
@@ -2,11 +2,19 @@ from pytz import timezone
 from datetime import datetime
 import time as tm
 import numpy as np
-import socket, sys, json, random, yaml
+import socket, sys, json, random, yaml, signal
 
 if(len(sys.argv) != 2):
     print('Usage: ".py [path to json config file]"')
     sys.exit()
+
+def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        for i in range(len(socketList)):
+            socketList[i].close()
+        #print(socketList)
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 PATH = sys.argv[1]
 configFile = open(PATH, "r");
@@ -22,30 +30,43 @@ sleepTime = configDict["sleepTime"]
 
 measurement = "temperature"
 mu, sigma = 25, 10
+socketList = []
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((dstIp, dstPort))
+if numberOfSocket > 1:
+    i = 0;
+    while True:
+        if i < numberOfSocket:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((dstIp, dstPort))
+            socketList.append(s)
+            print("socket %d connected" % s.fileno())
+            i += 1
 
-for i in range(numberOfData):
-
-    time = datetime.now(timezone('Asia/Tokyo'))
-    #temperature = int(np.random.normal(mu, sigma, 1))
-    temperature = random.randint(0,100)
-
-    json_body = {
-        "measurement": measurement,
-        "tags": {
-            "host": host,
-            "region": region
-        },
-        "time": str(time),
-        "fields": {
-            "value": temperature,
+elif numberOfSocket == 1:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((dstIp, dstPort))
+    for i in range(numberOfData):
+    
+        time = datetime.now(timezone('Asia/Tokyo'))
+        #temperature = int(np.random.normal(mu, sigma, 1))
+        temperature = random.randint(0,100)
+    
+        json_body = {
+            "measurement": measurement,
+            "tags": {
+                "host": host,
+                "region": region
+            },
+            "time": str(time),
+            "fields": {
+                "value": temperature,
+            }
         }
-    }
+    
+        msg = json.dumps(json_body)
+        msg += "\n"
+    
+        s.send(msg.encode('utf-8'))
+        tm.sleep(sleepTime)
 
-    msg = json.dumps(json_body)
-    msg += "\n"
-
-    s.send(msg.encode('utf-8'))
-    tm.sleep(sleepTime)
+    s.close()
