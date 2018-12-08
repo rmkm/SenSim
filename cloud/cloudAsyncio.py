7bb7750521a003b652089aa831e618c0ebd68f00
morishima311 <ryoryo311@gmail.com>
2018-12-08 16:53:10 +0900

    Added asyncio server socket script

---
+++ cloud/cloudAsyncio.py
@@ -0,0 +1,71 @@
+from influxdb import InfluxDBClient 
+from ast import literal_eval
+import socket, sys, json, random, yaml, signal
+import asyncio
+
+if(len(sys.argv) != 2):
+    print('Usage: ".py [path to json config file]"')
+    sys.exit()
+
+PATH = sys.argv[1]
+configFile = open(PATH, "r")
+configDict = yaml.load(configFile.read())
+
+host = configDict["host"]
+port = configDict["port"]
+if "database" in configDict:
+    DBname = configDict["database"]
+    client = InfluxDBClient(host='localhost', port=8086, database=DBname)
+
+
+def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+
+
+async def listener(loop, connection, address):
+    while True:
+        msg = await loop.sock_recv(connection, 1500)
+        if not msg:
+            break
+        msg = msg.decode('utf-8')
+        try:
+            dct = literal_eval(msg)
+        except SyntaxError:
+            print("Invalid JSON data")
+            continue
+        print("From {}: {}".format(address, msg))
+        dictList = []
+        dictList.append(dct)
+        try:
+            client.write_points(dictList)
+        except NameError:
+            continue
+    connection.close()
+
+
+async def worker(loop, sock):
+    while True:
+        conn, addr = await loop.sock_accept(sock)
+        print("New connection {}".format(addr))
+        loop.create_task(listener(loop, conn, addr))
+
+
+def main():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.setblocking(False)
+    sock.bind((host, port))
+    sock.listen()
+
+    event_loop = asyncio.get_event_loop()
+    try:
+        event_loop.run_until_complete(worker(event_loop, sock))
+    finally:
+        event_loop.close()
+
+
+if __name__ == '__main__':
+    main()
--- cloud/cloud_socket.py
+++ cloud/cloud_socket.py
@@ -1,10 +1,18 @@
 from influxdb import InfluxDBClient
 from ast import literal_eval
-import sys, socket, select
+import sys, socket, select, signal
 
 if(len(sys.argv) != 4):
     print('Usage: ".py [Host] [port] [DB name]" \n')
     sys.exit()
+
+def signal_handler(sig, frame):
+        print('You pressed Ctrl+C!')
+        for i in range(len(inputs)):
+            inputs[i].close()
+        #print(inputs)
+        sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 RECV_NUM = 100
 TIME_OUT = 10
@@ -15,9 +23,10 @@ DB = sys.argv[3]
 client = InfluxDBClient(host='localhost', port=8086, database=DB)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server.setblocking(0)
 server.bind((HOST, PORT))
-server.listen(5)
+server.listen(20)
 inputs = [server]
 outputs = []
 
@@ -29,11 +38,16 @@ while inputs:
             connection.setblocking(0)
             inputs.append(connection)
         else:
-            msg = s.recv(256).decode('utf-8')
+            msg = s.recv(1500).decode('utf-8')
             if not msg:
                 break
             print(msg)
-            dct = literal_eval(msg)
+            print('\n')
+            try:
+                dct = literal_eval(msg)
+            except SyntaxError:
+                print("Invalid JSON data")
+                continue
             dictList = []
             dictList.append(dct)
             client.write_points(dictList)
